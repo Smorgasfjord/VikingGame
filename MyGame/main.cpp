8b0f6f1170d8a10aabe41725f3563e1c15696c7b
Twoods01 <twoods0129@gmail.com>
2014-05-03 16:02:02 -0700

    Initial Commit

---
+++ MyGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyGame
+//
+//  Created by Taylor Woods on 5/3/14.
+//  Copyright (c) 2014 Taylor Woods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
+
