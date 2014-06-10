99f62e8e7771aa6518596efb6ca6c22e53cf171b
nclarke <nclarke@calpoly.edu>
2014-06-10 15:46:28 -0700

    falling snow particles

---
+++ shadows, dust, and falling snow/Components/Hammer.cpp
@@ -0,0 +1,187 @@
+//
+//  Hammer.cpp
+//  MyGame
+//
+//  Created by Taylor Woods on 5/4/14.
+//  Copyright (c) 2014 Taylor Woods. All rights reserved.
+//
+
+#include "Hammer.h"
+#define pi 3.14159
+
+Hammer::~Hammer()
+{
+   
+}
+Hammer::Hammer(std::string n) : 
+   GameObject(n)
+{
+}
+
+void Hammer::setInWorld(World * world, Bjorn *character, GameModel *hammerMod, GLHandles handles)
+{
+   GameModel simple = genSimpleModel(hammerMod);
+   initialize(hammerMod, 0, 0, handles);
+   setPos(character->getPos());
+   moveBy(glm::vec3(0, 0, .25));
+   scaleBy(glm::vec3(.3f));
+   rotateBy(glm::vec3(0, 180, -90));
+   this->world = world;
+   bjorn = character;
+   previousAngle = glm::vec3(0.0f);
+   hammerSide = true;
+   modelIdx = world->placeObject(this, &simple);
+   mountainSide = bjorn->mountainSide;
+}
+
+float d2r(float val)
+{
+   return val * (pi / 180.0f);
+}
+
+void Hammer::updatePos(float x, float y)
+{
+   moveBy(glm::vec3(-.5, 0, 0));
+}
+
+void Hammer::updateAngle(float x, float y)
+{
+   //Vector along which the hammer should end up
+   glm::vec3 currentAngle = getRot();
+   //Angle between desired vector and neutral hammer position (straight up)
+   float angle = atan2(x, y);
+   if(!isnan(angle) && !collision)
+   {
+      //Save the last angle
+      previousAngle = currentAngle;
+      //Rotation is flipped if the hammer is flipped
+      if(bjorn->mountainSide == MOUNT_FRONT || bjorn->mountainSide == MOUNT_BACK)
+      {
+         if(hammerSide)
+            setRotation(glm::vec3(currentAngle.x, currentAngle.y, -angle * (180.0 / pi)));
+         else
+            setRotation(glm::vec3(currentAngle.x, currentAngle.y, angle * (180.0 / pi)));
+      }
+      else
+      {
+         if(hammerSide)
+            setRotation(glm::vec3(angle * (180.0 / pi), currentAngle.y, currentAngle.z));
+         else
+            setRotation(glm::vec3(-angle * (180.0 / pi), currentAngle.y, currentAngle.z));
+      }
+   }
+}
+
+
+void Hammer::flip()
+{
+   if (!collision) {
+      hammerSide = !hammerSide;
+      rotateBy(glm::vec3(0, 180, 0));
+   }
+}
+
+// What hammer does
+void Hammer::step(double timeStep)
+{
+   setVelocity(bjorn->getVel());
+   moveBy(getVel()*(float)timeStep);
+   setPos(bjorn->getPos());
+
+   //Move the hammer to the proper position
+   if(bjorn->mountainSide == MOUNT_FRONT)
+      moveBy(glm::vec3(0, 0, -.2));
+   else if(bjorn->mountainSide == MOUNT_RIGHT)
+      moveBy(glm::vec3(-.2, 0, 0));
+   else if(bjorn->mountainSide == MOUNT_BACK)
+      moveBy(glm::vec3(0, 0, .2));
+   else
+      moveBy(glm::vec3(.2, 0, 0));
+   
+   //Update hammer rotation if we're on a different side of the mountain
+   if(mountainSide != bjorn->mountainSide)
+   {
+      if(mountainSide < bjorn->mountainSide)
+         rotateBy(glm::vec3(0, 90, 0));
+      else
+         rotateBy(glm::vec3(0, -90, 0));
+      mountainSide = bjorn->mountainSide;
+   }
+   
+   /*glm::vec3 hammerTip = getPos();
+   hammerTip.y += sin(d2r(getRot().z + 90));
+   if(hammerSide)
+      hammerTip.x -= cos(d2r(getRot().z + 90));
+   else
+      hammerTip.x += cos(d2r(getRot().z + 90));
+   if(world.detectCollision(hammerTip))
+   {
+      if(hammerSide && abs(previousAngle - getRot().z) > 4)
+      {
+         bjorn->launch(d2r(35));
+      }
+      else if(!hammerSide)
+         bjorn->suspend();
+      collision = true;
+   }
+   else
+   {
+      bjorn->unsuspend();
+   }*/
+   return;
+}
+
+// How the world reacts to Hommur
+void Hammer::update(double timeStep) {
+   glm::vec3 hammerTip, movedAngle;
+   CollisionData dat;
+   dat = world->checkCollision(this, modelIdx);
+   if (dat.hitObj.obj >= 0) {
+      printf("Hommur vertex %d hit platform %d face %d at the location (%f, %f, %f) with normal (%f, %f, %f) while moving in the direction (%f, %f, %f) after trying to move (%f, %f, %f)\n",
+                /*hammer.model.children[dat.thisObj.nod].name.c_str(), */dat.thisObj.tri, dat.hitObj.obj, dat.hitObj.tri,
+                dat.collisionPoint.x, dat.collisionPoint.y,dat.collisionPoint.z,dat.collisionNormal.x, dat.collisionNormal.y,dat.collisionNormal.z,
+                dat.collisionAngle.x, dat.collisionAngle.y,dat.collisionAngle.z,dat.collisionStrength.x, dat.collisionStrength.y,dat.collisionStrength.z);
+
+      if (hammerSide) { 
+         //m/s         = m/s           
+         activeForce = dat.collisionStrength*(float)GRAVITY/2.0f;
+         //                  m          /       s
+         bjorn->addVelocity(-activeForce);//(float)timeStep);
+         Sound::hammerSmash();
+      }
+      else if (!hammerSide && !collision) {
+         //m/s         = m/s                     * (no unit)           - (m/s^2                * s)
+         activeForce = dat.collisionStrength * dat.collisionNormal*(float)GRAVITY/2.0f;
+         bjorn->setVelocity(bjorn->getVel() * dat.collisionNormal);
+         pickAngle = glm::normalize(dat.collisionAngle);
+      }
+      else {
+         //m/s         = m/s                     - (m/s^2                * s)
+         activeForce = dat.collisionStrength*(float)GRAVITY/2.0f; 
+         //                  m/s
+         bjorn->addVelocity(-activeForce);//(float)timeStep);
+      }
+      collision = true;
+      //bjorn->suspend();
+      movedAngle = getRot();
+      setRotation(previousAngle);
+      if (dat.collisionStrength.x == 0.0f) dat.collisionStrength.x = 1.0f;
+      if (dat.collisionStrength.y == 0.0f) dat.collisionStrength.y = 1.0f;
+      if (dat.collisionStrength.z == 0.0f) dat.collisionStrength.z = 1.0f;
+      //rotateBy((movedAngle-previousAngle)*(dat.collisionAngle/dat.collisionStrength));
+      //addVelocity((dat.collisionAngle-dat.collisionStrength)/(float)timeStep);
+   }
+   else {
+      bjorn->unsuspend();
+      collision = false;
+   }
+   //moveBy(getVel()*(float)timeStep);
+   //setPos(glm::vec3(bjorn->getPos().x,bjorn->getPos().y + 0.4f,bjorn->getPos().z));
+   hammerTip = getPos();
+   hammerTip.y += sin(d2r(getRot().z + 90.0));
+   if(!hammerSide)
+      hammerTip.x -= cos(d2r(getRot().z + 90.0));
+   else
+      hammerTip.x += cos(d2r(getRot().z + 90.0));
+   //updateAngle(hammerTip.x,hammerTip.y);
+}
---
+++ shadows, dust, and falling snow/Utils/chunks.h
@@ -0,0 +1,178 @@
+#ifndef __CHUNKS_H__
+#define __CHUNKS_H__
+
+#include <vector>
+#include <map>
+
+#include "../Components/GameObject.h"
+#include "../Models/GameModel.h"
+#include </home/nclarke/Desktop/deps/glm/include/glm/glm.hpp>
+#include </home/nclarke/Desktop/deps/glm/include/glm/gtc/matrix_transform.hpp> //perspective, trans etc
+#include </home/nclarke/Desktop/deps/glm/include/glm/gtx/matrix_query.hpp> //perspective, trans etc
+#include </home/nclarke/Desktop/deps/glm/include/glm/gtc/type_ptr.hpp> //value_ptr
+
+#define CHUNK_SIZE 4.0f
+#define APP 0.001f
+#define BOOST 1.0001f
+#define COLL_LIMIT 1000000.0f
+
+using namespace std;
+
+class ChunkWorld;
+class Chunk;
+class MicroChunk;
+typedef struct chunk_data ChunkData;
+typedef struct obj_data ObjData;
+
+struct chunk_data {
+   int x; //x index
+   int y; //y index
+   int z; //z index
+};
+
+struct obj_data {
+   int obj; //object index
+   int nod; //object node
+   int mesh; //mesh index
+   int tri; //face index
+};
+
+class CollisionData {
+   public:
+      ~CollisionData() { }
+      CollisionData() 
+      {
+         hitObj.obj = -1;
+         hitObj.nod = -1;
+         hitObj.mesh = -1;
+         hitObj.tri = -1;
+      }
+      CollisionData(ObjData d, ObjData t, glm::vec3 p, glm::vec3 a, glm::vec3 n, glm::vec3 s) :
+         hitObj(d),
+         thisObj(t),
+         collisionPoint(p),
+         collisionAngle(a),
+         collisionNormal(n),
+         collisionStrength(s)
+      {
+      }
+      ObjData thisObj; //thisObj.tri is actually a vertex
+      ObjData hitObj;
+      glm::vec3 collisionPoint;
+      glm::vec3 collisionAngle;
+      glm::vec3 collisionStrength;
+      glm::vec3 collisionNormal;
+};
+
+// for mapping purposes
+
+bool operator==(const ChunkData a, const ChunkData b);
+bool operator!=(const ChunkData a, const ChunkData b);
+bool operator<(const ChunkData a, const ChunkData b);
+bool operator==(const ObjData a, const ObjData b);
+bool operator!=(const ObjData a, const ObjData b);
+bool operator<(const ObjData a, const ObjData b);
+
+glm::vec3 nextChunk(glm::vec3 pos, glm::vec3 ray, float scale);
+
+class MicroChunk {
+   public:
+      map<ObjData, glm::vec3> objects;
+      glm::vec3 minBound, maxBound, index;
+      int valid;
+
+      MicroChunk() :
+         valid(0)
+      {
+      }
+
+      MicroChunk(float x, float y, float z) :
+         valid(1),
+         index((int)x,(int)y,(int)z),
+         minBound(x/CHUNK_SIZE,y/CHUNK_SIZE,z/CHUNK_SIZE),
+         maxBound((x+1.0)/CHUNK_SIZE, (y+1.0)/CHUNK_SIZE,
+                  (z+1.0)/CHUNK_SIZE)
+      {
+      }
+
+      int isValid() {
+         return valid;
+      }
+};
+
+class Chunk {
+   public:
+      int empty;
+      glm::vec3 minBound, maxBound, index;
+
+      Chunk() :
+         index(0.0,0.0,0.0),
+         minBound(0.0,0.0,0.0),
+         maxBound(0.0,0.0,0.0),
+         empty(1)
+      {
+      }
+
+      Chunk(float x, float y, float z) :
+         index(x, y, z),
+         minBound(x, y, z),
+         maxBound(x+1.0, y+1.0, z+1.0),
+         empty(0)
+      {
+      }
+
+      int isValid() {
+         return !empty;
+      }
+
+};
+
+class ChunkWorld {
+   public:
+      map<ChunkData, Chunk> chunkMap;
+      map<ChunkData, MicroChunk> uChunkMap;
+      map<int, vector<glm::vec3> > objectMap;
+      Chunk invalidC;
+      MicroChunk invalidM;
+      glm::vec3 scale;
+      vector<GameObject> objects;
+      vector< vector<BufferContents> > models;
+//      vector<LightSource> lights;
+      int objCount;
+
+      ChunkWorld() :
+         objCount(0)
+      {
+      }
+
+      ChunkWorld(const int & width, const int & height, const int & depth) :
+         scale(width, height, depth),
+         objCount(0)
+      {
+      }
+      
+      void print(); 
+      Chunk & findChunk(float x, float y, float z);      
+      Chunk & addChunk(float x, float y, float z);
+      MicroChunk * findMicroChunk(float x, float y, float z); 
+      MicroChunk * addMicroChunk(float x, float y, float z);
+      void traceLine(glm::vec3 start, glm::vec3 end, ObjData dat);      
+      void traceTriangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, ObjData check);
+      void traceMesh(const BufferContents & geom, glm::mat4 trans, ObjData dat);
+      int traceNode(ObjectNode *nod, const vector<BufferContents> & geom, glm::mat4 trans, ObjData dat); 
+      int populate(GameObject *obj, const std::vector<BufferContents> & geom);
+      void repopulate(GameObject *obj, int objIndex);
+      glm::mat4 accumTransform(ObjectNode *node, glm::mat4 cumulative, int & currNod, int targetNode);
+      glm::mat4 findTransform(ObjData dat);
+      glm::vec3 interpolateNormal(float beta, float gamma, ObjData dat, glm::mat4 trans);
+      glm::vec3 findCollisionPoint(glm::vec3 path, glm::vec3 start, ObjData dat, glm::mat4 trans);
+      CollisionData checkMeshCollision(const BufferContents & geom, glm::mat4 newTrans, glm::mat4 oldTrans, ObjData & dat);
+      CollisionData checkNodeCollision(ObjectNode & newNod, ObjectNode & oldNod, const std::vector<BufferContents> & geom, glm::mat4 newTrans, glm::mat4 oldTrans, ObjData & dat);
+      CollisionData checkForCollision(GameObject *newObj, int objIndex);
+      void depopulate(int objIndex);
+      //int addMaterial(mat_t mat);      
+      //int addLight(LightSource light);
+};
+
+
+#endif
